326d0385d751c51b1234eeba44c812e1416c202b
Heli Ribeiro <heliribeiro2@gmail.com>
2021-12-30 00:31:17 -0300

    changes have been implemented

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,34 @@
-export function SideBar() {
+import { Button } from './Button';
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface sideBarProps {
+  genres: GenreResponseProps[],
+  selectedGenreId:number,
+  handleClickButton: Function
+}
+
+export function SideBar({genres, selectedGenreId, handleClickButton}:sideBarProps) {
+  return (
   // Complete aqui
+  <nav className="sidebar">
+  <span>Watch<p>Me</p></span>
+
+  <div className="buttons-container">
+    {genres.map(genre => (
+      <Button
+        key={String(genre.id)}
+        title={genre.title}
+        iconName={genre.name}
+        onClick={() => handleClickButton(genre.id)}
+        selected={selectedGenreId === genre.id}
+      />
+    ))}
+  </div>
+
+</nav>)
 }
